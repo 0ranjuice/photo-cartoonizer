1364f9ee8d25bd5cf7bf6d493627085f326ae513
JunHan901122 <hank901122@gmail.com>
2022-06-23 16:03:00 +0800

    test

--- app.py
+++ app.py
@@ -70,48 +70,6 @@ if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
 
-    
-def color_quantization(img, k):
-    # Transform the image
-    data = np.float32(img).reshape((-1, 3))
-
-    # Determine criteria
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 15, 0.001)
-
-    # Implementing K-Means
-    ret, label, center = cv2.kmeans(data, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
-    center = np.uint8(center)
-    result = center[label.flatten()]
-    result = result.reshape(img.shape)
-    return result
 
 
-def edge_mask(img, line_size, blur_value):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray_blur = cv2.medianBlur(gray, blur_value)
-    edges = cv2.adaptiveThreshold(gray_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, line_size, blur_value)
-    return edges
-
-
-image = cv2.imread("linda_1.jpg", -1)  # Read image
-
-# Parameter setting
-line_size = 7
-blur_value = 5
-total_color = 9
-
-edges = edge_mask(image, line_size, blur_value)
-
-quant_img = color_quantization(image, total_color)
-
-blurred = cv2.bilateralFilter(quant_img, d=3, sigmaColor=100, sigmaSpace=100)
-
-cartoon = cv2.bitwise_and(blurred, blurred, mask=edges)
-
-# Show image
-image = cv2.cvtColor(cartoon, cv2.COLOR_BGR2RGB)
-plt.figure(figsize=(18, 10), facecolor='black')
-plt.axis('off')
-plt.imshow(image)
-plt.show()
 
