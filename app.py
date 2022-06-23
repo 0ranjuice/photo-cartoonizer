8919582728347d2f53611db9feeb095f5a405867
JunHan901122 <hank901122@gmail.com>
2022-06-23 20:22:58 +0800

    init

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Jun  2 21:16:35 2021
+
 @author: Ivan
 版權屬於「行銷搬進大程式」所有，若有疑問，可聯絡ivanyang0606@gmail.com
 
@@ -8,33 +9,25 @@ Line Bot聊天機器人
 第一章 Line Bot申請與串接
 Line Bot機器人串接與測試
 """
+
 # 載入LineBot所需要的套件
-from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import *
-
-import cv2
-
-from matplotlib import pyplot as plt
-
-import numpy as np
-
 import os
+from flask import Flask, request, abort
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import *
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
 
 app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
-line_bot_api = LineBotApi(
-    'haz544FhuBP/m3xKqQ2B/RATT17n8Vwp380L9BfFj9UN+SpHMLUloC61EG8VE0e/qvU0BaYxdlHtvZoBjVYdrSDsK2ysH+YtyA/ohx8l2QGc3K6TmP8sGYsr6IvlG/B4ARQMbxRSntZMD6QJNBDdlwdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('haz544FhuBP/m3xKqQ2B/RATT17n8Vwp380L9BfFj9UN+SpHMLUloC61EG8VE0e/qvU0BaYxdlHtvZoBjVYdrSDsK2ysH+YtyA/ohx8l2QGc3K6TmP8sGYsr6IvlG/B4ARQMbxRSntZMD6QJNBDdlwdB04t89/1O/w1cDnyilFU=')
 # 必須放上自己的Channel Secret
 handler = WebhookHandler('5c5342dccfd566f8cb7420c77e73d6c5')
 
+# Push a message to me when everything is ready ( 放上 USER ID )
 line_bot_api.push_message('U1c7b721a4d53f66c456edf1d30681ae3', TextSendMessage(text='你可以開始了'))
 
 
@@ -53,7 +46,6 @@ def callback():
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
 
@@ -61,14 +53,15 @@ def callback():
 # 基本上程式編輯都在這個function #
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    message = event.message.text
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 
 
 # 主程式
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
 
 '''
 def color_quantization(img, k):
